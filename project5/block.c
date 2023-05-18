37edae449638086a39537c2ef205b9a441987416
Baleria Reyes <breyes815@gmail.com>
2023-05-18 08:10:06 -0700

    bread and bwrite functions implemented

--- project5/block.c
+++ project5/block.c
@@ -1,1 +1,20 @@
+#include <unistd.h>
+#include <fcntl.h>
+#include "image.h"
 
+#define BLOCK_SIZE 4096
+
+unsigned char *bread(int block_num, unsigned char *block) {
+  int file_descriptor = image_fd;
+  int offset = block_num * BLOCK_SIZE;  
+  lseek(file_descriptor, offset, SEEK_SET); 
+  read(file_descriptor, block, BLOCK_SIZE); // read from file_descriptor into block
+  return block;
+}
+
+void bwrite(int block_num, unsigned char *block) {
+  int file_descriptor = image_fd;
+  int offset = block_num * BLOCK_SIZE;
+  lseek(file_descriptor, offset, SEEK_SET);
+  write(file_descriptor, block, BLOCK_SIZE); // write from block into file_descriptor
+}
