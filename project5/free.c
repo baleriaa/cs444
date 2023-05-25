89398d017bf2d589d7fd8b5295a3437d113883c6
Baleria Reyes <breyes815@gmail.com>
2023-05-25 08:49:54 -0700

    add find_free

--- project5/free.c
+++ project5/free.c
@@ -1,4 +1,14 @@
+#include <stdlib.h>
+#include "block.h"
 
+int find_low_clear_bit(unsigned char x)
+{
+    for (int i = 0; i < 8; i++)
+        if (!(x & (1 << i)))
+            return i;
+    
+    return -1;
+}
 
 void set_free(unsigned char *block, int num, int set) {
   int byte_num = num / 8; 
@@ -12,11 +22,12 @@ void set_free(unsigned char *block, int num, int set) {
 
 }
 
-int find_low_clear_bit(unsigned char x)
-{
-    for (int i = 0; i < 8; i++)
-        if (!(x & (1 << i)))
-            return i;
-    
-    return -1;
-}
+int find_free(unsigned char *block) {
+  for (int i = 0; i < BLOCK_SIZE; i++) {
+    int lowest_clear_bit = find_low_clear_bit(block[i]);
+    if (lowest_clear_bit != -1) {
+      return i * 8 + lowest_clear_bit;
+    }
+  }
+  return -1;
+ }
