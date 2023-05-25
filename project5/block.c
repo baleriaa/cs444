f76f9e38c1394d27321aeeb6ad476472b156cc4a
Baleria Reyes <breyes815@gmail.com>
2023-05-25 08:47:38 -0700

    add alloc function

--- project5/block.c
+++ project5/block.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include "image.h"
-
-#define BLOCK_SIZE 4096
+#include "block.h"
+#include "free.h"
 
 unsigned char *bread(int block_num, unsigned char *block) {
   int file_descriptor = image_fd;
@@ -17,4 +17,16 @@ void bwrite(int block_num, unsigned char *block) {
   int offset = block_num * BLOCK_SIZE;
   lseek(file_descriptor, offset, SEEK_SET);
   write(file_descriptor, block, BLOCK_SIZE); // write from block into file_descriptor
+}
+
+int alloc(void) {
+  unsigned char block[BLOCK_SIZE];
+  bread(2, block);
+  int free_block = find_free(block);
+  if (free_block == -1) {
+    return -1;
+  }
+  set_free(block, free_block, 1);
+  bwrite(2, block);
+  return free_block;
 }
